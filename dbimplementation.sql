fc71a107c6e9b5f6fe09de59dcc3f44cc5729873
MaxwellLondon <maxwelllondon99@gmail.com>
2023-03-02 18:33:52 -0800

    adding working databse implementaiton

--- dbimplementation.sql
+++ dbimplementation.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS Movies;
-DROP TABLE IF EXISTS Actors;
-DROP TABLE IF EXISTS Directors;
-DROP TABLE IF EXISTS Rating;
-DROP TABLE IF EXISTS User;
-DROP TABLE IF EXISTS Role;
+DROP TABLE IF EXISTS Movies
+DROP TABLE IF EXISTS Actors
+DROP TABLE IF EXISTS Directors
+DROP TABLE IF EXISTS Rating
+DROP TABLE IF EXISTS User
+DROP TABLE IF EXISTS Role
 
 CREATE TABLE Movies (
     id INT,
@@ -11,47 +11,47 @@ CREATE TABLE Movies (
     genre VARCHAR(50),
     runtime VARCHAR(50),
     title VARCHAR(100),
-    language VARCHAR(50)
-);
+    language VARCHAR(50),
+	Primary Key (id)
+)
 
 CREATE TABLE Actors (
-    id INT
+    id INT,
     firstName VARCHAR(50),
     lastName VARCHAR(50),
-    PRIMARY KEY id
-
-);
+    PRIMARY KEY (id)
+)
 
 CREATE TABLE Directors (
-    id INT
+    id INT,
     firstName VARCHAR(50),
     lastName VARCHAR(50),
-    PRIMARY KEY id
-);
+    PRIMARY KEY (id)
+)
 
-CREATE TABLE User (
+CREATE TABLE appUser (
     id INT,
     username VARCHAR(50),
     isAdmin BOOLEAN,
-    PRIMARY KEY id
-);
+    PRIMARY KEY (id)
+)
 
 CREATE TABLE Rating (
     score INT,
     rating_id INT,
     user_id INT,
     movie_id INT,
-    time DATETIME
+    time timestamp,
     PRIMARY KEY (rating_id, user_id, movie_id),
-    FOREIGN KEY user_id REFERENCES Users(id),
-    FOREIGN KEY movie_id REFERENCES Movies(id)
-);
+    FOREIGN KEY (user_id) REFERENCES appUser(id),
+    FOREIGN KEY (movie_id) REFERENCES Movies(id)
+)
 
 CREATE TABLE Role (
-    m.id INT,
-    a.id INT,
+    movie_id INT,
+    actor_id INT,
     role VARCHAR(50),
-    PRIMARY KEY (m.id, a.id),
-    FOREIGN KEY a.id REFERENCES Actors(id),
-    FOREIGN KEY m.id REFERENCES Movies(id)
-);
+    PRIMARY KEY (movie_id, actor_id),
+    FOREIGN KEY (actor_id) REFERENCES Actors(id),
+    FOREIGN KEY (movie_id) REFERENCES Movies(id)
+)
