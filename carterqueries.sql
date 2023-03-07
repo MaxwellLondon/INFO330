f6854ab2be3fc1d47fa53a931dc25e9c0a3f14d6
Oscar <oscarmaldonadoj23@gmail.com>
2023-03-06 16:46:08 -0800

    max queries cleaned up and running

--- carterqueries.sql
+++ carterqueries.sql
@@ -3,7 +3,7 @@ SELECT a.id, a.firstName, a.lastName
 FROM Actors a, Role r, Movies m
 WHERE a.id = r.actor_id
 AND m.id = r.movie_id
-AND m.release >= '03062018'
+AND m.release >= '03-06-2018'
 GROUP BY a.id, a.firstName, a.lastName
 ORDER BY count(m.id) DESC
 LIMIT 1;
@@ -19,8 +19,8 @@ ORDER BY avg(r.score) DESC
 LIMIT 5;
 
 --11
-SELECT a.id, a.firstName, a.lastName
-FROM Actors a, Movie m, Rating r, Role o
+SELECT a.id, a.firstName, a.lastName, avg(r.score)
+FROM actors a, movies m, Rating r, role o
 WHERE a.id = o.actor_id
 AND m.id = o.movie_id
 AND m.id = r.movie_id
@@ -29,10 +29,9 @@ ORDER BY avg(r.score) ASC
 LIMIT 5;
 
 --18
-SELECT m.id, m.title
-FROM Movie m, appUser u, Rating r
+SELECT m.id, m.title, m.runtime
+FROM Movies m, appUser u, Rating r
 WHERE m.id = r.movie_id
 AND u.id = r.user_id
-AND u.username = 'austenfan'
-ORDER BY m.runtime
-LIMIT 1;
+ORDER BY cast(m.runtime as INTEGER) desc;
+
--- dbimplementation.sql
+++ dbimplementation.sql
@@ -64,6 +64,7 @@ CREATE TABLE Directs (
     FOREIGN KEY (movie_id) REFERENCES Movies(id)
 );
 
+-- Commands to insert data into tables genericly
 --  create temporary table temp (csv schema);                                                                                                       user_id int,                                                                                                         movie_id int,                                                                                                        time timestamp                                                                                                       );
 -- CREATE TABLE
 -- oscarj23_db=> \copy temp from 'absolute filepath to file' csv header;
@@ -72,3 +73,41 @@ CREATE TABLE Directs (
 -- INSERT 0 10
 -- oscarj23_db=> select * from table (checking if they inserted)
 -- drop table temp;
+
+-- queries
+--8
+-- SELECT a.id, a.firstName, a.lastName
+-- FROM Actors a, Role r, Movies m
+-- WHERE a.id = r.actor_id
+-- AND m.id = r.movie_id
+-- AND m.release >= '03-06-2018'
+-- GROUP BY a.id, a.firstName, a.lastName
+-- ORDER BY count(m.id) DESC
+-- LIMIT 1;
+
+-- --10
+-- SELECT d.id, d.firstName, d.lastName, avg(r.score)
+-- FROM Directors d, Directs s, Movies m, Rating r
+-- WHERE d.id = s.director_id
+-- AND m.id = s.movie_id
+-- AND m.id = r.movie_id
+-- GROUP BY d.id, d.firstName, d.lastName
+-- ORDER BY avg(r.score) DESC
+-- LIMIT 5;
+
+-- --11
+-- SELECT a.id, a.firstName, a.lastName, avg(r.score)
+-- FROM actors a, movies m, Rating r, role o
+-- WHERE a.id = o.actor_id
+-- AND m.id = o.movie_id
+-- AND m.id = r.movie_id
+-- GROUP BY a.id, a.firstName, a.lastName
+-- ORDER BY avg(r.score) ASC
+-- LIMIT 5;
+
+-- --18
+-- SELECT m.id, m.title, m.runtime
+-- FROM Movies m, appUser u, Rating r
+-- WHERE m.id = r.movie_id
+-- AND u.id = r.user_id
+-- ORDER BY cast(m.runtime as INTEGER) desc;
